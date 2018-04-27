879e88fd9ed9658c87c38a3bd00f6863d335b782
Allan James Lapid <ajLapid718@gmail.com>
2018-04-26 22:01:23 -0400

    Successfully established index pages for players and for teams

--- app.js
+++ app.js
@@ -12,8 +12,6 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-// Consider using something to render (like Nunjucks) as well as serving up static folder information;
-
 // GET-PUT-POST-DELETE-CRUD;
 app.use(router);
 
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const models = require('../models/index.js');
+const Team = models.Team;
+const Player = models.Player;
 
 module.exports = router;
 
 // GET-PUT-POST-DELETE-CRUD;
+
+// The homepage aka LocalHost:3000;
 router.get('/', function(req, res, next) {
   res.send('Hello world! This is the home page.');
 });
 
-router.get('/teamOne', function(req, res, next) {
-  res.send('Hello world! This is the team page.');
+// An index page listing all of the teams;
+router.get('/allTeams', function(req, res, next) {
+  let allTeams = Team.findAll().then(function(teams) {
+    res.json(teams);
+  }).catch(err => console.log(err));
+});
+
+// An index page listing all of the players;
+router.get('/allPlayers', function(req, res, next) {
+  let allPlayers = Player.findAll().then(function(players) {
+    res.json(players);
+  }).catch(err => console.log(err));
 });

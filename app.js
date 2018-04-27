996290b59eb16c03d49da3c9480e764ca239f4f4
Allan James Lapid <ajLapid718@gmail.com>
2018-04-26 20:31:35 -0400

    Create and visit sample routes

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
+// Requirements;
 const express = require('express');
 const app = express();
 
+// GET-PUT-POST-DELETE-CRUD;
+app.get('/', function(req, res, next) {
+  res.send('Hello world! This is the home page.');
+});
+
+app.get('/teamOne', function(req, res, next) {
+  res.send('Hello world! This is the team page.');
+});
+
+// Listener;
 app.listen(3000, function() {
   console.log('Server is up and running!');
 })

1505705cef9806a19cb75f3c43873e7f95e33327
Allan James Lapid <ajLapid718@gmail.com>
2018-04-26 21:36:05 -0400

    Is this how to seed though

--- seed.js
+++ seed.js
@@ -1,1 +1,38 @@
 const models = require('/.models/index.js');
+const Team = models.team;
+const Player = models.player;
+
+// Team Seeds;
+Team.create({
+  cityName: 'Miami',
+  teamName: 'Heat'
+});
+
+Team.create({
+  cityName: 'New York',
+  teamName: 'Knicks'
+});
+
+Team.create({
+  cityName: 'Boston',
+  teamName: 'Celtics'
+});
+
+// Player Seeds;
+Player.create({
+  fullName: 'Dwyane Wade',
+  jerseyNum: 3,
+  position: 'SG'
+});
+
+Player.create({
+  fullName: 'Kristaps Porzingis',
+  jerseyNum: 6,
+  position: 'PF'
+});
+
+Player.create({
+  fullName: 'Kyrie Irving',
+  jerseyNum: 11,
+  position: 'PG'
+});

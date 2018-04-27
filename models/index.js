cb0818f6a1ae04f84a554fb00f7729be49f999db
Allan James Lapid <ajLapid718@gmail.com>
2018-04-26 21:17:03 -0400

    Finish defining the Team and the Player models

--- models/index.js
+++ models/index.js
@@ -3,8 +3,32 @@ const db = new Sequelize('postgres://localhost:5432/basketball', {logging: false
 
 let Team = db.define('team', {
 
-})
+  cityName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+
+  teamName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+
+});
 
 let Player = db.define('player', {
 
-})
+  fullName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+
+  position: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+
+  jerseyNum: {
+    type: Sequelize.INTEGER,
+  }
+
+});

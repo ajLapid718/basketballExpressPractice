29e0b8b5910791eba413b31e9ed01e047a5557cd
Allan James Lapid <ajLapid718@gmail.com>
2018-04-26 20:59:21 -0400

    Write down next steps before making models

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const router = require ('./routes/index.js');
 
+// REMINDER: In the near future, like after this commit, require models and sync to the database;
+
 // Middleware;
 app.use(morgan('dev'));
 app.use(bodyParser.json());

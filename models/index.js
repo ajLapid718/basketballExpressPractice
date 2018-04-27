ea398d62e4e88a32f786aefdd10da0b61998f185
Allan James Lapid <ajLapid718@gmail.com>
2018-04-26 21:09:32 -0400

    Begin to define tables

--- models/index.js
+++ models/index.js
@@ -1,2 +1,10 @@
 const Sequelize = require('sequelize');
 const db = new Sequelize('postgres://localhost:5432/basketball', {logging: false});
+
+let Team = db.define('team', {
+
+})
+
+let Player = db.define('player', {
+
+})
